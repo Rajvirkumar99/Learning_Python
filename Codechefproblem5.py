551502ff4fc24f33b188c77942aeac2b4b7f3cbd
RAJVIR KUMAR <84075116+Rajvirkumar99@users.noreply.github.com>
2022-11-27 18:47:05 +0530

    Create Codechefproblem5.py
    #Learning Motivation

---
+++ Codechefproblem5.py
@@ -0,0 +1,26 @@
+# cook your dish here
+# Anshu 
+# Arvind 
+# N is no of test case 
+""" 
+logic if they want choclate in equal 
+
+so divide a choclate  
+
+choclate %2==0 print yes 
+
+choclate %2!=0  print no 
+
+"""
+
+N= int (input())
+
+for i in range (1,N+1):
+    
+    choclate=int(input())
+    
+    if choclate%2==0:
+        
+        print("Yes")
+    else :
+        print("No")
